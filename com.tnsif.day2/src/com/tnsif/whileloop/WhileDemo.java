453f2c1287fa97dc8fb00793220791085a6b915f
my pc <my pc@LAPTOP-DDO0MQ8N>
2024-08-11 13:10:08 +0530

    commit 1

---
+++ com.tnsif.day2/src/com/tnsif/whileloop/WhileDemo.java
@@ -0,0 +1,14 @@
+package com.tnsif.whileloop;
+
+public class WhileDemo {
+	public static void main(String[] args) 
+	{
+		int i=99;
+		while(i<=100)
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+
+}
